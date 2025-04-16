b10dcda44fce9793857d0194735b1dd3f82544dc
danidedona <daniela_dedona@brown.edu>
2025-04-16 17:15:38 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+
+import Template from "./Template";
+
+function App() {
+  return (
+    <div className="font-sans" style={{ paddingTop: "var(--navbar-height)" }}>
+      <Template />;
+    </div>
+  );
+}
+
+export default App;
